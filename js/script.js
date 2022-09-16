7899d900d717bc237d59193697efd9c8663f1988
tonnymuchui <tonykanyingah@gmail.com>
2022-09-16 17:05:34 +0300

    added dynamic for main page and featured speakers

--- js/script.js
+++ js/script.js
@@ -32,3 +32,169 @@ function hideB() {
 
 moreButton.addEventListener('click', showB);
 lessButton.addEventListener('click', hideB);
+
+const cards = [
+  {
+    id: 1,
+    imageM: './images/Picture3.png',
+    heading3: 'Tonnny',
+    heading5: 'FirstName LastName',
+    cardDescription:
+      'Listen to the speaker from various countries about sharing and opening',
+  },
+  {
+    id: 2,
+    imageM: './images/Picture3.png',
+    heading3: 'FirstName LastName',
+    heading5: 'FirstName LastName',
+    cardDescription:
+      'Listen to the speaker from various countries about sharing and opening',
+  },
+  {
+    id: 3,
+    imageM: './images/Picture3.png',
+    heading3: 'FirstName LastName',
+    heading5: 'FirstName LastName',
+    cardDescription:
+      'Listen to the speaker from various countries about sharing and opening',
+  },
+  {
+    id: 4,
+    imageM: './images/Picture3.png',
+    heading3: 'FirstName LastName',
+    heading5: 'FirstName LastName',
+    cardDescription:
+      'Listen to the speaker from various countries about sharing and opening',
+  },
+];
+
+const page2Card = cards.map(
+  (card, index) => `
+  <section class="page2 ${index}">
+  <div class="prg1">
+      <h3 class="program2">Featured Speakers</h3>
+      <hr class="underline">
+  </div>
+
+  <div class="speakerCards">
+      <div class="card-lec">
+          <img src="${card.imageM}" alt="lecture">
+
+          <div class="feature-card">
+              <h3>${card.heading3}</h3>
+              <h5>${card.heading5}</h5>
+              <p>Listen to the speaker from various countries about sharing and opening</p>
+          </div>
+      </div>
+      <div class="card-lec">
+          <img src="./images/Picture3.png" alt="lecture">
+
+          <div class="feature-card">
+              <h3>Lecturer</h3>
+              <h5>Speaker</h5>
+              <p>Listen to the speaker from various countries about sharing and opening</p>
+          </div>
+      </div>
+      <div class="dis-none">
+          <button class="show-more" type="button">MORE &#x2228;</button>
+      </div>
+  </div>
+
+  <div class="speakerCards1 dis-none1">
+      <div class="card-lec">
+          <img src="./images/Picture3.png" alt="lecture">
+          <div class="feature-card">
+              <h3>Lecturer1</h3>
+              <h5>Speaker</h5>
+              <p>Listen to the speaker from various countries about sharing and opening</p>
+          </div>
+      </div>
+
+      <div class="card-lec ">
+          <img src="./images/Picture3.png" alt="lecture">
+
+          <div class="feature-card">
+              <h3>Lecturer2</h3>
+              <h5>Speaker</h5>
+              <p>Listen to the speaker from various countries about sharing and opening</p>
+          </div>
+
+      </div>  
+      <div class="card-lec">
+          <img src="./images/Picture3.png" alt="lecture">
+          <div class="feature-card">
+              <h3>Lecturer3</h3>
+              <h5>Speaker</h5>
+              <p>Listen to the speaker from various countries about sharing and opening</p>
+          </div>
+      </div>
+      <div class="card-lec">
+          <img src="./images/Picture3.png" alt="lecture">
+          <div class="feature-card">
+              <h3>Lecturer</h3>
+              <h5>Speaker</h5>
+              <p>Listen to the speaker from various countries about sharing and opening</p>
+          </div>
+      </div>
+      <div class="dis-none">
+          <button class="show-less" type="button">LESS &#x22C0;</button>
+      </div>
+  </div>
+</section>
+`,
+);
+const workCard = document.querySelector('.popup');
+workCard.innerHTML += page2Card;
+
+const programCards = [
+  {
+    id: 1,
+    title: 'Food Systems& nutrition',
+    imageM: './images/lecture.png',
+    cardDescription:
+      'Listen to the speaker from various countries about sharing and opening.',
+  },
+  {
+    id: 2,
+    title: 'Women in Agriculture',
+    imageM: './images/lecture.png',
+    cardDescription:
+      'Listen to the speaker from various countries about sharing and opening.',
+  },
+  {
+    id: 3,
+    title: 'Agtech',
+    imageM: './images/lecture.png',
+    cardDescription:
+      'Listen to the speaker from various countries about sharing and opening.',
+  },
+  {
+    id: 4,
+    title: 'Regional trade',
+    imageM: './images/lecture.png',
+    cardDescription:
+      'Listen to the speaker from various countries about sharing and opening.',
+  },
+  {
+    id: 5,
+    title: 'Agri-investment',
+    imageM: './images/lecture.png',
+    cardDescription:
+      'Listen to the speaker from various countries about sharing and opening.',
+  },
+];
+
+const programCard = programCards.map(
+  (card, index) => `
+  <div class="programCards ${index}">
+  <div class="card">
+      <img class="${index}" src="${card.imageM}" alt="lecture">
+      <h3>${card.title}</h3>
+      <p>${card.cardDescription}</p>
+  </div>
+  </div>
+  `,
+).join('');
+
+const program = document.querySelector('.programCards');
+program.innerHTML += programCard;
